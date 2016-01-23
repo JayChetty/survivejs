185e76afaa084687fa30b6d09c3db2f2c73e79db
Jay Chetty <jaypaulchetty@gmail.com>
2016-01-23 10:21:54 +0000

    add delete styling

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -29,11 +29,20 @@ export default class App extends React.Component {
 
     return (
       <div>
-        <button onClick={this.addNote}>+</button>
-        <Notes notes={notes} onEdit={this.editNote} />
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <Notes notes={notes}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote} />
       </div>
     );
 
+  };
+
+
+  deleteNote = (id) => {
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    });
   };
 
   addNote = () => {
